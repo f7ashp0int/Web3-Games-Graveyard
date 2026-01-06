48e3521c0e92ed7c0868a818ef36df7a0c9301f1
f7ashp0int <rjarivi@gmail.com>
2026-01-06 10:51:10 +0530

    chore: migrate to @tailwindcss/vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/Web3-Games-Graveyard/',
+      plugins: [
+        tailwindcss(),
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
